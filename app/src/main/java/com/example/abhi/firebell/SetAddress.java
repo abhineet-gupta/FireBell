2f9e5582fc6ddb883ee040d5920846d2cf475591
Durantula91 <mduran@student.unimelb.edu.au>
2017-09-18 11:39:14 +1000

    Location page

---
+++ app/src/main/java/com/example/abhi/firebell/SetAddress.java
@@ -0,0 +1,13 @@
+package com.example.abhi.firebell;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SetAddress extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_set_address);
+    }
+}
