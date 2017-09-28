5ccf596bdfc94ef6af6af29ac290eaad01f964e6
BigWillyJJ <wilkins.leong@yahoo.com.au>
2017-09-28 21:46:07 +1000

    added share preferences functionality for settings switches, -wilks

--- app/src/main/java/com/example/abhi/firebell/Settings.java
+++ app/src/main/java/com/example/abhi/firebell/Settings.java
@@ -3,6 +3,7 @@ package com.example.abhi.firebell;
 import android.content.Intent;
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
@@ -12,7 +13,11 @@ import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.RelativeLayout;
+import android.widget.Switch;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class Settings extends AppCompatActivity {
 
@@ -62,8 +67,13 @@ public class Settings extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
 
+        // Get widgets from layout file
+        final RelativeLayout rl = (RelativeLayout) findViewById(R.id.content);
         mTextMessage = (TextView) findViewById(R.id.textViewSettAddrDescr);
         displayAddress();
+
+        Switch switch_PushNotif = (Switch) findViewById(R.id.switchSettPushNotf);
+        Switch switch_PhoneAlarm = (Switch) findViewById(R.id.switchSettPhoneAlarm);
 
         BottomNavigationView navigation =
                 (BottomNavigationView) findViewById(R.id.bottom_navigation);
@@ -80,7 +90,75 @@ public class Settings extends AppCompatActivity {
             }
         });
 
-        //SharedPreferences sharedPreferences = getSharedPreferences(fileNameString, MODE_PRIVATE);
+
+        // get shared preferences file for the switches in settings page.
+        Context context = getApplicationContext();
+        final SharedPreferences sharedPref = context.getSharedPreferences(
+                getString(R.string.pref_file_key), Context.MODE_PRIVATE);
+
+        //set notif switch listener and update to storage
+        switch_PushNotif.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked){
+                    // If the switch button is on, test...
+                    // rl.setBackgroundColor(Color.parseColor("#FF80DFB0"));
+
+                    SharedPreferences.Editor editor = sharedPref.edit();
+                    editor.putBoolean(getString(R.string.pref_notifications_key), true);
+                    editor.commit();
+
+                    // Show the switch button checked status as toast message
+                    Toast.makeText(getApplicationContext(),
+                            "Push Notifications is on", Toast.LENGTH_LONG).show();
+                }
+                else {
+                    // If the switch button is off, test ...
+                // rl.setBackgroundColor(Color.parseColor("#ed252f"));
+
+                SharedPreferences.Editor editor = sharedPref.edit();
+        editor.putBoolean(getString(R.string.pref_notifications_key), false);
+        editor.commit();
+
+        // Show the switch button checked status as toast message
+        Toast.makeText(getApplicationContext(),
+                "Push Notifications off", Toast.LENGTH_LONG).show();
+    }
+}
+        });
+
+        //set alarms switch listener and update to storage
+        switch_PhoneAlarm.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked){
+                    // If the switch button is on, test...
+                    // rl.setBackgroundColor(Color.parseColor("#FF80DFB0"));
+
+                    SharedPreferences.Editor editor = sharedPref.edit();
+                    editor.putBoolean(getString(R.string.pref_alarm_key), true);
+                    editor.commit();
+
+                    // Show the switch button checked status as toast message
+                    Toast.makeText(getApplicationContext(),
+                            "Alarms on", Toast.LENGTH_LONG).show();
+                }
+                else {
+                    // If the switch button is off, test ...
+                    // rl.setBackgroundColor(Color.parseColor("#ed252f"));
+
+                    SharedPreferences.Editor editor = sharedPref.edit();
+                    editor.putBoolean(getString(R.string.pref_alarm_key), false);
+                    editor.commit();
+
+                    // Show the switch button checked status as toast message
+                    Toast.makeText(getApplicationContext(),
+                            "Alarm off", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
     }
 
     @Override
