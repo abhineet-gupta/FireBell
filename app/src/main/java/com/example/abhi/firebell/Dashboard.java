962955c03b9fb7aae8ee4e0bed3061fa8021b3b2
Abhineet Gupta <abhineetg@student.unimelb.edu.au>
2017-10-09 01:14:44 +1100

    News button works | Emergency calling works

--- app/src/main/java/com/example/abhi/firebell/Dashboard.java
+++ app/src/main/java/com/example/abhi/firebell/Dashboard.java
@@ -1,6 +1,7 @@
 package com.example.abhi.firebell;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
@@ -12,6 +13,8 @@ import android.view.View;
 import android.widget.Button;
 
 public class Dashboard extends AppCompatActivity {
+
+    private String NEWS_URL = "http://www.mfb.vic.gov.au/News.html";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,7 +53,6 @@ public class Dashboard extends AppCompatActivity {
             public void onClick(View view) {
                 Intent statusIntent = new Intent(Dashboard.this, Status.class);
                 startActivity(statusIntent);
-                finish();
             }
         });
 
@@ -61,7 +63,30 @@ public class Dashboard extends AppCompatActivity {
             public void onClick(View view) {
                 Intent emergenciesIntent = new Intent(Dashboard.this, Emergencies.class);
                 startActivity(emergenciesIntent);
-                finish();
+            }
+        });
+
+        Button fireNewsBtn = findViewById(R.id.fireNews);
+        fireNewsBtn.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                Intent newsInt = new Intent();
+                newsInt.setAction(Intent.ACTION_VIEW);
+                newsInt.addCategory(Intent.CATEGORY_BROWSABLE);
+                newsInt.setData(Uri.parse(NEWS_URL));
+                startActivity(newsInt);
+            }
+        });
+
+        final Button callEmergencyBtn = findViewById(R.id.buttonEmergency);
+        callEmergencyBtn.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                Intent callEmergencyInt = new Intent(Intent.ACTION_DIAL);
+                callEmergencyInt.setData(Uri.parse("tel:000"));
+                startActivity(callEmergencyInt);
             }
         });
     }
